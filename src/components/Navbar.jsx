94c9de48bde9e108bea46148cfa51912afde59ea
David Vasek <david.vasek51@gmail.com>
2021-08-13 10:59:54 -0500

    modified navbar to include linking to login, sign up, and search feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,20 @@ const Navbar = () => {
     <div className="navbar">
       <Router>
         <nav>
+          <a href="/" className="brand">
+            <span className="brand__react">[Name]</span>
+          </a>
+        </nav>
+        <nav className="nav">
           <Link to="/">Home</Link>
-          <Link to="/">Home</Link>
-          <Link to="/">Home</Link>
-          <Link to="/">Home</Link>
+          <Link to="/">Explore</Link>
+          <Link to="/">Forums</Link>
+          <Link to="/">Communities</Link>
+          <input className="search" placeholder="Search" />
+        </nav>
+        <nav className="nav-right">
+          <Link to="/">Login |</Link>
+          <Link to="/"> Sign Up</Link>
         </nav>
         <Switch>
           <Route exact path="/">
