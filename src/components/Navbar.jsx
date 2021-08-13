3f0653fc8d7e0e67bf8c8edbffa383890c384cad
David Vasek <david.vasek51@gmail.com>
2021-08-13 14:29:41 -0500

    commented out imports to work on splashpage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,34 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
 
 const Navbar = () => {
-    return(
-        <div className="Navbar">
-            <Router>
-                <nav>
-                    <Link to="/">Home</Link>
-                </nav>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <Router>
+        <nav>
+          <a href="/" className="brand">
+            <span className="brand__react">[Name]</span>
+          </a>
+        </nav>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/">Explore</Link>
+          <Link to="/">Forums</Link>
+          <Link to="/">Communities</Link>
+          <input className="search" placeholder="Search" />
+        </nav>
+        <nav className="nav-right">
+          <Link to="/">Login |</Link>
+          <Link to="/"> Sign Up</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default Navbar;
