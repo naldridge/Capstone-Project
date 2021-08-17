6144ec47fe6af951368ff07574d101a2950659ed
1mvnnie <80409842+1mvnnie@users.noreply.github.com>
2021-08-17 10:41:14 -0500

    created post component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
+import axios from 'axios';
+
 function Home() {
+
+  const defaultChannels = () => {
+    axios({
+      method: 'GET',
+      // url: display list of top channels
+    })
+  }
+
+  const defaultPosts = () => {
+    axios({
+      method: 'GET',
+      // url: display most liked posts?
+    })
+  }
+
+  // const defaultComments = 
+
     return (
-      <div>
-        <h2>Welcome to ____!</h2>
-        <h4>The Web Developer's Community</h4>
-      </div>
+
+
+      <main className='home'>
+        <div className='header'>
+          <h1>Welcome to ____!</h1>
+          <h3>A Space For Developers Of All Kinds!</h3>
+        </div>
+        <div className="channels">
+
+        </div>
+        <div className='posts'>
+
+        </div>
+
+        {/* div for top comments? some other info? */}
+
+      </main>
     );
   }
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const url = `https://localhost:300/add`;
+
+const Post = () => {
+
+    const sendToServer = () => {
+        axios({
+            method: 'POST',
+            url: url,
+        })
+        .then(response => this.setState({postId: response.data.id})
+        )
+        .catch(error => {
+            console.log('ERROR: ', error)
+        })
+    }
+
+    const [userPost, setUserPost] = useState(' ')
+    
+    return (
+        <>
+        <div className='postCard'>
+            <label htmlFor="post">Create a Post
+                <input 
+                type="text"
+                value={userPost}
+                onChange={(e) => {setUserPost(e.target.value)}}
+                 placeholder='Post Something...' />
+                <button 
+                type='submit'
+                onClick={() => {
+                    sendToServer()
+                }}
+                >
+                Post
+                </button>
+            </label>
+
+        </div>
+        </>
+    )
+}
+
+export default Post;
