263fc446e4702d3c26629d109c9e03d9e8dd8dac
David Vasek <david.vasek51@gmail.com>
2021-08-17 11:30:17 -0500

    added redirect for logout button

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -3,7 +3,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
-  return isAuthenticated && <button onClick={() => logout()}>Log Out</button>;
+  return (
+    isAuthenticated && (
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
+    )
+  );
 };
 
 export default LogoutButton;
