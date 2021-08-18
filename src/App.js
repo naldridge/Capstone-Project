e674758ed4502c091c9c7dda4bcca49eea962e64
Nick Aldridge <naldridge1360@gmail.com>
2021-08-18 14:10:10 -0500

    prepping the files to use, installed dependencies

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 // import HomePage from "./components/Homepage";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { isLoading } = useAuth0();
+/*   const { isLoading } = useAuth0();
 
-  if (isLoading) return <div>Loading...</div>;
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <Sidebar />
-      {/* <HomePage /> */}
-    </div>
-  );
-}
+  if (isLoading) {
+    return (
+      <div>Loading...</div>
+    );
+  } else { */
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <Sidebar />
+        {/* <HomePage /> */}
+      </div>
+    );
+  }
 
 export default App;
