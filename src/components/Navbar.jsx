406b32e9fff4945fa26020ab0fe82faf6d14ea57
David Vasek <david.vasek51@gmail.com>
2021-08-18 11:16:33 -0500

    changed layout to incorporate chakra UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,68 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 import Home from "./Home";
+import { Flex, Heading, Spacer, Box, Button } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <Router>
-        <nav>
-          <a href="/" className="brand">
-            <span className="brand__react">[Name]</span>
-          </a>
-        </nav>
-        <nav className="nav">
-          <Link to="/">Home</Link>
-          <Link to="/">Explore</Link>
-          <Link to="/">Forums</Link>
-          <Link to="/">Communities</Link>
-          <input className="search" placeholder="Search" />
-        </nav>
-        <nav className="nav-right">
-          <Link to="/">Login |</Link>
-          <Link to="/"> Sign Up</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    // <div className="navbar">
+    //   <Router>
+    //     <div className="leftNav">
+    //       <Nav className="justify-content-start" activeKey="/home">
+    //         <Nav.Item>
+    //           <Nav.Link href="/home">[ NAME ]</Nav.Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </div>
+    //     <div className="centerNav">
+    //       <Nav className="justify-content-center" activeKey="/home">
+    //         <Nav.Item>
+    //           <Nav.Link href="/home">Explore</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="link-1">Forums</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="link-2">Communities</Nav.Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </div>
+    //     <div className="rightNav">
+    //       <Nav className="justify-content-end" activeKey="/home">
+    //         <Nav.Item>
+    //           <Nav.Link href="/home">Log In</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="link-1">Sign Up</Nav.Link>
+    //         </Nav.Item>
+    //         <Nav.Item>
+    //           <Nav.Link eventKey="link-2">About The Team</Nav.Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </div>
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
+    //     </Switch>
+    //   </Router>
+    // </div>
+    <Flex p="2">
+      <Box p="4">
+        <Heading size="md" ml="8">
+          fetch-dev()
+        </Heading>
+      </Box>
+      <Spacer />
+      <Box>
+        <Button colorScheme="purple" mr="5">
+          Sign Up
+        </Button>
+        <Button colorScheme="purple" mr="4">
+          Log in
+        </Button>
+      </Box>
+    </Flex>
   );
 };
 
