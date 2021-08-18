63c4c37d7f13d69e5a0d1e5ccf7bb02d388fb4f3
David Vasek <david.vasek51@gmail.com>
2021-08-18 15:12:31 -0500

    added splashpage hook

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 // import { Button, Card } from "react-bootstrap";
 import SplashPage from "./components/SplashPage";
+import Homepage from "./components/SplashPage";
 
 function App() {
   return (
     <div className="App">
       <SplashPage />
+      <Homepage />
     </div>
   );
 }
