023947183fac25924dfc744634bf0e50089286a1
David Vasek <david.vasek51@gmail.com>
2021-08-18 14:07:04 -0500

    adding dark mode button

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,10 +1,15 @@
 // theme.js
-// export default {
-//   colors: {
-//     transparent: "transparent",
-//     black: "#000",
-//     white: "#fff",
-//     purple: #322659,
-//     // ...
-//   },
-// };
+
+// 1. import `extendTheme` function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const Theme = extendTheme({ config });
+
+export default Theme;
