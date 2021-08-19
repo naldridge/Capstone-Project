9bbe1b9528aa96a46be48a5a7078c26b52ae8f3f
David Vasek <david.vasek51@gmail.com>
2021-08-19 10:27:04 -0500

    made navbar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,22 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
+
 import Home from "./Home";
 import { Flex, Heading, Spacer, Box, Button } from "@chakra-ui/react";
-import { IconButton } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    // <div className="navbar">
-    //   <Router>
-    //     <div className="leftNav">
-    //       <Nav className="justify-content-start" activeKey="/home">
-    //         <Nav.Item>
-    //           <Nav.Link href="/home">[ NAME ]</Nav.Link>
-    //         </Nav.Item>
-    //       </Nav>
-    //     </div>
-    //     <div className="centerNav">
-    //       <Nav className="justify-content-center" activeKey="/home">
-    //         <Nav.Item>
-    //           <Nav.Link href="/home">Explore</Nav.Link>
-    //         </Nav.Item>
-    //         <Nav.Item>
-    //           <Nav.Link eventKey="link-1">Forums</Nav.Link>
-    //         </Nav.Item>
-    //         <Nav.Item>
-    //           <Nav.Link eventKey="link-2">Communities</Nav.Link>
-    //         </Nav.Item>
-    //       </Nav>
-    //     </div>
-    //     <div className="rightNav">
-    //       <Nav className="justify-content-end" activeKey="/home">
-    //         <Nav.Item>
-    //           <Nav.Link href="/home">Log In</Nav.Link>
-    //         </Nav.Item>
-    //         <Nav.Item>
-    //           <Nav.Link eventKey="link-1">Sign Up</Nav.Link>
-    //         </Nav.Item>
-    //         <Nav.Item>
-    //           <Nav.Link eventKey="link-2">About The Team</Nav.Link>
-    //         </Nav.Item>
-    //       </Nav>
-    //     </div>
-    //     <Switch>
-    //       <Route exact path="/">
-    //         <Home />
-    //       </Route>
-    //     </Switch>
-    //   </Router>
-    // </div>
-    <Flex p="2" className="headerNav">
+    <Flex p="3" position="sticky" className="headerNav">
       <Box p="4">
-        <Heading size="md" ml="10" color="white">
+        <Heading size="md" ml="7" color="white">
           fetch-dev()
         </Heading>
       </Box>
+
       <Spacer />
 
       <Box>
-        <Button colorScheme="brand" mr="5">
-          Sign Up
-        </Button>
-        <Button colorScheme="brand" mr="5" p="5">
+        <Button mr="5">Sign Up</Button>
+        <Button mr="5" p="5">
           Log in
         </Button>
       </Box>
