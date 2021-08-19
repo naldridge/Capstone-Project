f2283d3790643abc2b6e119f604c1b09d6878fad
1mvnnie <80409842+1mvnnie@users.noreply.github.com>
2021-08-19 11:47:02 -0500

    'adds fetch functions for posts and comments'

--- src/components/pages/Feed.jsx
+++ src/components/pages/Feed.jsx
@@ -1,27 +1,38 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Feed = () => {
     const [posts, setPosts] = useState([ ]);
-    // const [comments, setComments] = useState([ ]);
-   
-    axios({
-        method: 'GET',
-        url: 'http://localhost:3333/post/showposts',
-    })
-    .then((response) => {
-        console.log(response);
-        setPosts(response.data)
-    })
-    .catch(error => {
-        alert(error)
-    })
+    const [comments, setComments] = useState([ ]);
+    
+    const getPosts = 'http://localhost:3333/post/showposts';
+    const getComments = 'http://localhost333/comment/show_comments';
 
-    for (const post in posts) {
+    const postsCall = axios.get(getPosts);
+    const commentCall = axios.get(getComments);
+
+    useEffect(() => {
+        axios.all([postsCall, commentCall])
+        .then(axios.spread((...responses) => {
+            const postResponse = responses[0];
+            const commentResponse = responses[1];
+
+            setPosts(postResponse);
+            setComments(commentResponse)
+        }))
+        .catch(error => {
+            alert(error)
+        })
+    });
+
+    for (const post in posts && commentFeedback in comments) {
         return (
             <div className="postContainer">
                 {post}
+                <div className="commentContainer">
+                    {commentFeedback}
+                </div>
             </div>
         )
     }
