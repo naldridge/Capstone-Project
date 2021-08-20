c664ce1362548ae23a7e6b1420c41f25dd08287d
David Vasek <david.vasek51@gmail.com>
2021-08-20 18:41:19 -0500

    deleted repeated import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+
 import ProfilePage from "./components/ProfilePage";
 // import HomePage from "./components/Homepage";
 import "bootstrap/dist/css/bootstrap.min.css";
