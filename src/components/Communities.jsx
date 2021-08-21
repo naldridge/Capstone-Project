3ca7b3b67903e0f3278f201aae79d99d092b4f2d
David Vasek <david.vasek51@gmail.com>
2021-08-21 01:10:20 -0500

    added containers to community page and changed format. Need to still map data into each

--- src/components/Communities.jsx
+++ src/components/Communities.jsx
@@ -1,41 +1,68 @@
-import React, { Component } from 'react';
-import { FiSidebar } from 'react-icons/fi';
-import Sidebar from './Sidebar';
+import React, { Component } from "react";
+import { FiSidebar } from "react-icons/fi";
+import Sidebar from "./Sidebar";
+import "./Communities.css";
 
 class Communities extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            channel: [],
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      channel: [],
+    };
+  }
 
-    componentDidMount() {
-        this._fetchDefaultChannels();
-        console.log("state: ", this.state.channel);
-    }
+  componentDidMount() {
+    this._fetchDefaultChannels();
+    console.log("state: ", this.state.channel);
+  }
 
-    async _fetchDefaultChannels() {
-        const url = "http://localhost:3333/channel/default";
-        const response = await fetch(url).then(response => response.json());
+  async _fetchDefaultChannels() {
+    const url = "http://localhost:3333/channel/default";
+    const response = await fetch(url).then((response) => response.json());
 
-        const defaultChannels = response;
-        console.log("default channels: ", defaultChannels);
+    const defaultChannels = response;
+    console.log("default channels: ", defaultChannels);
 
-        this.setState({
-            channel: [defaultChannels]
-        });
-    }
+    this.setState({
+      channel: [defaultChannels],
+    });
+  }
 
-    render() {
-        const { channel } = this.state;
+  render() {
+    const { channel } = this.state;
 
-        return (
-            <div className="DefaultChannels">
-                {!!channel.length > 0 ? (<Sidebar channel={channel} />) : null}
-            </div>
-        );
-    }
+    return (
+      <div className="DefaultChannels">
+        <div className="mainDisplay">
+          <div className="communityName">
+            [Name of Community] Posted by [User] 3 months ago
+          </div>
+          <div className="postTitle">This Is The Title</div>
+          <div className="postBody">
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+          </div>
+          <div className="postComments">4 comments</div>
+        </div>
+        <div className="commentDisplay">
+          <div className="commentorTitle">Comment as davey</div>
+          <form className="commentForm">
+            <textarea
+              className="commentInput"
+              type="text"
+              placeholder="what are your thoughts"
+            />
+            <input className="submitComment" type="submit" value="comment" />
+          </form>
+        </div>
+        {/* {!!channel.length > 0 ? (<Sidebar channel={channel} />) : null} */}
+      </div>
+    );
+  }
 }
 
 export default Communities;
