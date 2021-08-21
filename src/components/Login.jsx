0c6e1aeca3451a3e275398c486e15eecf0f86ce6
David Vasek <david.vasek51@gmail.com>
2021-08-21 01:09:28 -0500

    added auth0 to login button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Login = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
   return (
-    <button className="loginButton" onClick={() => loginWithRedirect()}>
+    <button className="loginButton" onClick={() => loginWithPopup()}>
       Log In
     </button>
   );
