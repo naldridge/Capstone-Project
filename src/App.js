47b5eb71f545bc7a88144fe5cd610f4b20f06681
David Vasek <david.vasek51@gmail.com>
2021-08-21 01:08:15 -0500

    took off unneeded imports, added imports to community page, and changed the structure of route/switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
-// import Auth0ProviderWithHistory from "./components/auth0Provider";
+// import Auth0Provider from "./components/auth0Provider";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
-import { useAuth0 } from "@auth0/auth0-react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import {
   StreamApp,
   NotificationDropdown,
@@ -21,59 +17,36 @@ import "react-activity-feed/dist/index.css";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import DisplayChannel from "./components/DisplayChannel";
-// import HomePage from "./components/Homepage";
+import HomePage from "./components/Homepage";
+import Communities from "./components/Communities";
+import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 function App() {
- /*  const { isLoading } = useAuth0();
+  /*  const { isLoading } = useAuth0();
   const stream = React.Component;
 
   if (isLoading) return <div>Loading...</div>; */
 
   return (
-    <div>
-      <StreamApp
-        apiKey="dz5f4d5kzrue"
-        appId="102254"
-        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZHJ5LXZvaWNlLTQiLCJleHAiOjE2MjkzMTYxMzJ9.kdE2XExoVYvuT66HOrnH2muPCwfd2vWg0CxkQWs4qsw"
-      >
-        <NotificationDropdown notify />
-        <FlatFeed
-          notify
-          Activity={(props) => (
-            <Activity
-              {...props}
-              Footer={() => (
-                <div style={{ padding: "8px 16px" }}>
-                  <LikeButton {...props} />
-                </div>
-              )}
-            />
-          )}
-        />
-      </StreamApp>
-
-      {/* <Auth0ProviderWithHistory> */}
-      <div className="App">
-        <LoginButton />
-        <LogoutButton />
-        <Profile />
-      </div>
-      {/* </Auth0ProviderWithHistory> */}
+    <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          <Route path={`/channel/:slug`}>
-            <DisplayChannel />
-          </Route>
-        </Switch>
+        <div className="container">
+          <div className="sideBar">
+            <Sidebar channel={null} />
+          </div>
+          <div className="mainContainer">
+            <Switch>
+              <Route exact path={`/`} component={HomePage} />
+              <Route path={`/communities`} component={Communities} />
+              <Route path={`/login`} component={Login} />
+              <Route path={`/channel/:slug`} component={DisplayChannel} />
+              <Route path={`/profile`} component={Profile} />
+            </Switch>
+          </div>
+        </div>
       </Router>
-
-      <Sidebar channel={null} />
-
-
-      {/* <HomePage /> */}
     </div>
   );
 }
