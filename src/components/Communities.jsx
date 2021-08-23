4f382d2d2fd99510892cefb4eb7b089d1ac27b23
Brittany Fisher <bribreana7@gmail.com>
2021-08-23 15:18:04 -0400

    sizing corrected

--- src/components/Communities.jsx
+++ src/components/Communities.jsx
@@ -33,111 +33,120 @@ class Communities extends Component {
 
     return (
       <div className="DefaultChannels">
-        <div className="mainDisplay">
-          <div className="communityName">
-            [Name of Community] Posted by [User] 3 months ago
-          </div>
-          <div className="postTitle">This Is The Title</div>
-          <div className="postBody">
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-          </div>
-          <div className="postComments">4 comments</div>
-        </div>
-        <div className="commentDisplay">
-          <div className="commentorTitle">Comment as davey</div>
-          <form className="commentForm">
-            <textarea
-              className="commentInput"
-              type="text"
-              placeholder="what are your thoughts"
-            />
-            <input className="submitComment" type="submit" value="comment" />
-          </form>
-
+        <div className="channelItem">
           <div className="mainDisplay">
             <div className="communityName">
-              [Name of Community] Posted by [User] 2 months ago
+              [Name of Community] Posted by [User] 3 months ago
             </div>
-
-            <div className="postTitle"> This is the Title</div>
+            <div className="postTitle">This Is The Title</div>
             <div className="postBody">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti
-              mollitia ut obcaecati? Reprehenderit minima iste error eaque
-              repudiandae nobis, architecto provident corporis voluptatem
-              nesciunt facilis qui iure impedit numquam consectetur.
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est,
             </div>
+            <div className="postComments">4 comments</div>
           </div>
-          <div className="postComments"> 8 comments</div>
           <div className="commentDisplay">
-            <div className="commentorTitle">Comment as bri</div>
+            <div className="commentorTitle">Comment as davey</div>
             <form className="commentForm">
               <textarea
                 className="commentInput"
                 type="text"
-                placeholder="What are your thoughts"
+                placeholder="what are your thoughts"
               />
               <input className="submitComment" type="submit" value="comment" />
             </form>
-
-            <div className="mainDisplay">
-              <div className="communityName">
-                [Name of Community] Posted by [User] 12 days ago
-              </div>
-
-              <div className="postTitle"> This is the Title</div>
-              <div className="postBody">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptas asperiores, corporis saepe, enim quod magnam blanditiis
-                consectetur earum fugiat pariatur ipsam quae nulla beatae?
-                Inventore excepturi numquam repellendus temporibus magnam.
-              </div>
-            </div>
           </div>
-          <div className="postComments">2 Comments</div>
+        </div>
+        <div className="channelItem">
+          <div className="mainDisplay">
+            <div className="communityName">
+              [Name of Community] Posted by [User] 3 months ago
+            </div>
+            <div className="postTitle">This Is The Title</div>
+            <div className="postBody">
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est,
+            </div>
+            <div className="postComments">4 comments</div>
+          </div>
           <div className="commentDisplay">
-            <div className="commentorTitle">Comment as immanuel</div>
-            <form className="commentsForm">
+            <div className="commentorTitle">Comment as davey</div>
+            <form className="commentForm">
               <textarea
                 className="commentInput"
                 type="text"
-                placeholder="What are your thoughts"
+                placeholder="what are your thoughts"
               />
               <input className="submitComment" type="submit" value="comment" />
             </form>
-            <div className="mainDisplay">
-              <div className="communityName">
-                [Name of Community Posted by [User] 1 day ago]
-              </div>
-
-              <div className="postTitle">This is the Title</div>
-              <div className="postBody">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
-                maiores nesciunt excepturi accusamus error, enim rerum dolor
-                temporibus fugit voluptatibus aut voluptatum perferendis
-                dolores. Deleniti debitis at distinctio. Doloribus, provident.
-              </div>
+          </div>
+        </div>
+        <div className="channelItem">
+          <div className="mainDisplay">
+            <div className="communityName">
+              [Name of Community] Posted by [User] 3 months ago
             </div>
-            <div className="postComments"> 3 comments</div>
-            <div className="commentDisplay">
-              <div className="commentorTitle">Comment as nick</div>
-              <form className="commentForm">
-                <textarea
-                  className="commentInput"
-                  type="text"
-                  placeholder="What are your thoughts"
-                />
-                <input
-                  className="submitComment"
-                  type="submit"
-                  value="comment"
-                />
-              </form>
+            <div className="postTitle">This Is The Title</div>
+            <div className="postBody">
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est,
             </div>
+            <div className="postComments">4 comments</div>
+          </div>
+          <div className="commentDisplay">
+            <div className="commentorTitle">Comment as davey</div>
+            <form className="commentForm">
+              <textarea
+                className="commentInput"
+                type="text"
+                placeholder="what are your thoughts"
+              />
+              <input className="submitComment" type="submit" value="comment" />
+            </form>
+          </div>
+        </div>
+        <div className="channelItem">
+          <div className="mainDisplay">
+            <div className="communityName">
+              [Name of Community] Posted by [User] 3 months ago
+            </div>
+            <div className="postTitle">This Is The Title</div>
+            <div className="postBody">
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est,
+            </div>
+            <div className="postComments">4 comments</div>
+          </div>
+          <div className="commentDisplay">
+            <div className="commentorTitle">Comment as davey</div>
+            <form className="commentForm">
+              <textarea
+                className="commentInput"
+                type="text"
+                placeholder="what are your thoughts"
+              />
+              <input className="submitComment" type="submit" value="comment" />
+            </form>
           </div>
         </div>
       </div>
