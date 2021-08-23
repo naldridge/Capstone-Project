59baf975b2628555f9dbffebebdf07684e8d741d
David Vasek <david.vasek51@gmail.com>
2021-08-23 14:07:29 -0500

    deleted image folder and added community button to community page

--- src/components/Communities.jsx
+++ src/components/Communities.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { FiSidebar } from "react-icons/fi";
 import Sidebar from "./Sidebar";
 import "./Communities.css";
+import CreateCommunityButton from "./CreateCommunityButton";
 
 class Communities extends Component {
   constructor(props) {
@@ -32,35 +33,41 @@ class Communities extends Component {
     const { channel } = this.state;
 
     return (
-      <div className="DefaultChannels">
-        <div className="mainDisplay">
-          <div className="communityName">
-            [Name of Community] Posted by [User] 3 months ago
-          </div>
-          <div className="postTitle">This Is The Title</div>
-          <div className="postBody">
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-          </div>
-          <div className="postComments">4 comments</div>
+      <>
+        <div>
+          <CreateCommunityButton />
         </div>
-        <div className="commentDisplay">
-          <div className="commentorTitle">Comment as davey</div>
-          <form className="commentForm">
-            <textarea
-              className="commentInput"
-              type="text"
-              placeholder="what are your thoughts"
-            />
-            <input className="submitComment" type="submit" value="comment" />
-          </form>
+        <div className="DefaultChannels">
+          <div className="mainDisplay">
+            <div className="communityName">
+              [Name of Community] Posted by [User] 3 months ago
+            </div>
+            <div className="postTitle">This Is The Title</div>
+            <div className="postBody">
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est,
+            </div>
+            <div className="postComments">4 comments</div>
+          </div>
+          <div className="commentDisplay">
+            <div className="commentorTitle">Comment as davey</div>
+            <form className="commentForm">
+              <textarea
+                className="commentInput"
+                type="text"
+                placeholder="what are your thoughts"
+              />
+              <input className="submitComment" type="submit" value="comment" />
+            </form>
+          </div>
+          {/* {!!channel.length > 0 ? (<Sidebar channel={channel} />) : null} */}
         </div>
-        {/* {!!channel.length > 0 ? (<Sidebar channel={channel} />) : null} */}
-      </div>
+      </>
     );
   }
 }
